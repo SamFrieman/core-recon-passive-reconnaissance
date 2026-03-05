fc295db4598cf76dce7e0e77b631369b6681f8fe
Sam Frieman <81392225+SamFrieman@users.noreply.github.com>
2026-03-04 19:57:12 -0500

    Update main.py
    space error

--- backend/main.py
+++ backend/main.py
@@ -1409,4 +1409,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-   uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
