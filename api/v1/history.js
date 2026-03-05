0180069d8d563100e68b5ee77a81cd251aa8f844
Sam Frieman <81392225+SamFrieman@users.noreply.github.com>
2026-03-05 12:27:27 -0500

    Delete api directory

--- api/v1/history.js
+++
@@ -1,5 +0,0 @@
-module.exports = function handler(req, res) {
-  // Serverless functions are stateless; return empty history placeholder
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  return res.status(200).json({ scans: [] });
-}
